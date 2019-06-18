dcbac762ad1d4460bfbcd3877d6005bd63e8a4b9
James Brisbourne <jb2177@york.ac.uk>
2019-06-18 16:45:55 +0100

    Made 2D vector change

--- code/1d_diffusion.cpp
+++ code/1d_diffusion.cpp
@@ -22,8 +22,9 @@ struct input_variables {
 
 //only here to stop main() freaking out 
 input_variables input();
-void print_config(const std:: string &file_name, double dt, double time0, const vector<double>& T_array);
-vector<double> time_integration(double dt, double dx, const vector<double>& T_array);
+void print_config(const std:: string &file_name, double dt, double time0, const vector<vector<double>>& T_array);
+vector<vector<double>> time_integration(double dt, double dx, const vector<vector<double>>& T_array);
+//double prefactor(const vector<vector<double>>& T_array, int iter); 
 
 
 int main() {
@@ -32,31 +33,45 @@ int main() {
 	int runs = 50;
 	double time0 = 0.0;
 
-	printf("%f\n",constant );
+	const input_variables inputs = input();
+
 	std::stringstream integration_timestep;
 	integration_timestep.str("");
 	integration_timestep << dt;
 
-
-
-	const input_variables inputs = input();
+	//integration_timestep << inputs.dt; THIS WILL FIX FILE NAME ISSUE BUT BREAK OTHER THINGS
 
 	std::string outFileTemperatureProfile;	
 	outFileTemperatureProfile.append("Temperature_Profile_dt");
 	outFileTemperatureProfile.append(integration_timestep.str());
 	outFileTemperatureProfile.append(".dat");
 
-
 	// This is how to delare a vector (basically a better array), with N elements all of value 0
 	
-	vector<double> T_array(inputs.N,0);
+	vector<vector<double>> T_array(4, vector<double> (inputs.N, 0));
+
+	for (int i = 0; i < inputs.N; i++) {
+		T_array[1][i] = 0.5;
+		T_array[2][i] = 1000;
+		T_array[3][i] = 4000;
+}
+
+	// for (int i = 0; i < T_array.size(); i++) {
+ //    	for (int j = 0; j < T_array[i].size(); j++) {
+ //        	std::cout << T_array[i][j];
+ //    	}
+ //    	std::cout << std::endl;
+	// }
 
 	// changes the value of the first element in the array
-	T_array[0] = 37;
-	
+	T_array[0][0] = 37.0;
+	print_config(outFileTemperatureProfile, inputs.dt, time0, T_array);
+
 	
 	for (int i = 0; i < inputs.runs;i++) { // iterates for as many times as specified in the 'runs' section of the input file
 		T_array = time_integration(inputs.dt, inputs.dx, T_array); // runs euler step
+		printf("%f", time0);
+
 		time0 += inputs.dt; // increments time
 		print_config(outFileTemperatureProfile, inputs.dt, time0, T_array);
 	}
@@ -64,34 +79,48 @@ int main() {
 	return 0;
 }
 
-
-vector<double> time_integration(double dt, double dx, const vector<double>& T_array) {
+vector<vector<double>> time_integration(double dt, double dx, const vector<vector<double>>& T_array) {
 // does the Euler integration step on 'T_array' 
 // the new value for each position is loaded into 
 // T_update, finally T_update becomes T_array ready 
 // for the next iteration
-	const int N = T_array.size();
-	vector<double> T_update(N);
-
+	const int N = T_array[0].size();
+	vector<vector<double>> T_update(4, vector<double> (N, 0));
+	T_update = T_array;
 	// boundary conditions
-	T_update[0] = T_array[0];
-	T_update[N] = T_array[N];
+	T_update[0][0] = T_array[0][0];
+	T_update[0][N] = T_array[0][N];
+
+	//printf("%f", T_array[0][0]);
 
 	for (int i = 1; i < (N-1); i++) {
 		// euler step
-		T_update[i] = T_array[i] + constant * dt * (T_array[i-1] - 2*T_array[i] + T_array[i+1])/(dx*dx);
+		//printf("%f", T_array[2][1]);
+		//double constant = prefactor(T_array, iter);
+		//printf("%f", constant);
+		T_update[0][i] = T_array[0][i] + (T_array[1][i]/( T_array[2][i] + T_array[3][i])) * dt * (T_array[0][i-1] - 2*T_array[0][i] + T_array[0][i+1])/(dx*dx);
+
 	}
 
 
 	return T_update;
 }
 
-void print_config(const std:: string &file_name, double dt, double time0, const vector<double>& T_array) {
+// double prefactor(const vector<vector<double>>& T_array, int iter) {
+// 	//prefactor_result = thermal_conductivity/(density + specific_heat);
+// 	double prefactor_result = 0.0;
+// 	//printf("%f", T_array[1][1]);
+// 	prefactor_result = T_array[1][iter]/( T_array[2][iter] +T_array[3][iter]);
+// 	//printf("%f",prefactor_result);
+// 	return prefactor_result;
+// }
+
+void print_config(const std:: string &file_name, double dt, double time0, const vector<vector<double>>& T_array) {
 // handles all the needs of outputting data to a file.
 // the output file name is specified by the arguement used
 // when calling the function.
 
-    if (abs(time0 - dt) < (dt/100.0)) {
+    if (abs(time0 - dt) < (-dt/100.0)) {
     	std::ofstream outFile(file_name);
     	std::cout << "write to file " << file_name << std::endl;
     }
@@ -103,18 +132,22 @@ void print_config(const std:: string &file_name, double dt, double time0, const 
     }  
     
  
-    for (int i=0;i<T_array.size();i++){
+    for (int i=0;i < T_array[0].size();i++){
     // this line specifies what gets output to the file, "\t" indicates tabs between the data
-    outFile << time0 << "\t" << i << "\t" << T_array[i] << "\t"<< std::endl;
+    outFile << time0 << "\t" << i << "\t" << T_array[0][i] << "\t"<< std::endl;
         }
+
+    // for (int i = 0; i < T_array.size(); i++) {
+    // 	for (int j = 0; j < T_array[i].size(); j++) {
+    // 		outFile << time0 << "\t" << i << "\t" << T_array[0][i] << "\t"<< std::endl;
+    // 	}
+    // 	outFile << std::endl;
+    // }
 
     outFile << std::endl;
 
     outFile.close();
 }
-
-
-
 
 input_variables input() {
 
