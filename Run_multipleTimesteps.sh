5f160d94a28d910a5232df5003f0b6f4670fe352
James Brisbourne <jb2177@york.ac.uk>
2019-07-05 12:45:01 +0100

    Just a file rename from 'Run_multipleConditions.sh' to 'Run_multipleTimesteps.sh' for slightly more clarity

---
+++ Run_multipleTimesteps.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+fmain=$PWD
+fexe=executable
+exe=1d_test
+plot1=utility/plot.gnu
+
+Results=Run_1
+
+
+
+if [ -d $Results ]
+then
+	echo "The folder $Results exist. change name or delete the folder"
+	exit
+else
+mkdir $Results
+fi
+
+cd $fexe
+
+for dt0 in `seq 1 10`
+do
+cd $fmain/$fexe
+dt=$(echo $dt0 | awk '{print $1/100.0}' )
+echo "time step is: " $dt
+f1=dt_$dt
+
+if [ -d $f1 ]
+then
+	"folder $f1 exist"
+	exit
+else
+	mkdir $f1
+fi
+
+
+cd $f1
+mkdir $fexe
+mkdir input
+cp $fmain/$fexe/$exe $fexe/
+cp $fmain/$plot1 $fexe/
+cat << EOF > input/input.in
+time_step:      $dt         [-]
+discretisation_x:       1.0         [-]
+N:          100                  [-]
+run:            1000             [-]        
+EOF
+
+cd $fexe
+echo ""
+echo "!!!!!!!!!!!!!!!!"
+echo "runing code ...."
+time ./$exe
+echo "finished, now plotting"
+gnuplot plot.gnu
+cp Temperature_Profile.png $fmain/$Results/Temperature_Profile_dt$dt.png
+echo "finished time step of ", $dt
+echo ""
+echo ""
+
+done
+
+
